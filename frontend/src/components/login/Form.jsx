4b2b253e72910d5b5db84dde665c5a7763c2fec7
kyoko <kevin.kbr123@gmail.com>
2022-10-20 21:03:48 -0400

    Add login functionality to login component

--- frontend/src/components/login/Form.jsx
+++ frontend/src/components/login/Form.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../features/user/userSlice";
 
 const Form = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userData = { username, password };
+    dispatch(loginUser(userData));
+  };
+
   return (
     <div className="form d-flex justify-content-center my-0">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-usuario d-flex flex-column my-4">
           <label>USUARIO</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </div>
         <div className="form-contraseña d-flex flex-column my-4">
           <label>CONTRASEÑA</label>
-          <input type="pasword" />
+          <input
+            type="pasword"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
         <div className="d-flex justify-content-center">
           <button>LOGIN</button>
