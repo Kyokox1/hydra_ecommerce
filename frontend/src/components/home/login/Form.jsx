fd4be24c83d54ee3f3f7d34ec39338d70669345c
kyoko <kevin.kbr123@gmail.com>
2022-10-26 15:05:10 -0400

    Add Login functionality

--- frontend/src/components/home/login/Form.jsx
+++ frontend/src/components/home/login/Form.jsx
@@ -3,48 +3,59 @@ import React, { useState } from "react";
 //redux
 import { useSelector, useDispatch } from "react-redux";
 import { signClear } from "../../../features/sigIn/signSlice";
-import { loginUser } from "../../features/user/userSlice";
+
+// router
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-  const estado = useSelector((state) => state.sign);
-  const dispatch = useDispatch();
+	const estado = useSelector((state) => state.sign);
+	const user = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const userData = { username, password };
-    dispatch(loginUser(userData));
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (username === user.username && password === user.password) {
+			console.log("correcto!!!");
+			navigate("/productos");
+		}
+	};
 
-  return (
-    <div className="form d-flex justify-content-center my-0">
-      <form onSubmit={handleSubmit}>
-        <div className="form-usuario d-flex flex-column my-4">
-          <label>USUARIO</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-contraseña d-flex flex-column my-4">
-          <label>CONTRASEÑA</label>
-          <input
-            type="pasword"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div className="d-flex justify-content-center">
-          <button>LOGIN</button>
-        </div>
-        <h5 className="text-center">Olvidaste la contraseña</h5>
-        <h5 onClick={() => dispatch(signClear(!estado.estado))} className="text-center crear-cuenta">Crea una cuenta</h5>
-      </form>
-    </div>
-  );
+	return (
+		<div className="form d-flex justify-content-center my-0">
+			<form onSubmit={handleSubmit}>
+				<div className="form-usuario d-flex flex-column my-4">
+					<label>USUARIO</label>
+					<input
+						type="text"
+						value={username}
+						onChange={(event) => setUsername(event.target.value)}
+					/>
+				</div>
+				<div className="form-contraseña d-flex flex-column my-4">
+					<label>CONTRASEÑA</label>
+					<input
+						type="pasword"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+					/>
+				</div>
+				<div className="d-flex justify-content-center">
+					<button>LOGIN</button>
+				</div>
+				<h5 className="text-center">Olvidaste la contraseña</h5>
+				<h5
+					onClick={() => dispatch(signClear(!estado.estado))}
+					className="text-center crear-cuenta"
+				>
+					Crea una cuenta
+				</h5>
+			</form>
+		</div>
+	);
 };
 
 export default Form;
--- frontend/src/components/home/signIn/FormSign.jsx
+++ frontend/src/components/home/signIn/FormSign.jsx
@@ -1,35 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+
+// redux
+import { useDispatch } from "react-redux";
+import { signClear } from "../../../features/sigIn/signSlice";
+import { registerUser } from "../../../features/user/userSlice";
 
 const FormSign = () => {
-  return (
-    <div className="formSignIn">
-      <form>
-        <div className="d-flex justify-content-center">
-          <button>Crear Cuenta</button>
-        </div>
-        <div className="formSignIn-usuario d-flex flex-column my-4">
-          <label>USUARIO</label>
-          <input type="text" />
-        </div>
-        <div className="formSignIn-usuario d-flex flex-column my-4">
-          <label>CORREO ELECTRONICO</label>
-          <input type="text" />
-        </div>
-        <div className="formSignIn-contraseña d-flex flex-column my-4">
-          <label>CONTRASEÑA</label>
-          <input type="pasword" />
-        </div>
-        <div className="formSignIn-usuario d-flex flex-column my-4">
-          <label>REPETIR CONTRASEÑA</label>
-          <input type="text" />
-        </div>
-        <div className="formSignIn-usuario d-flex flex-column my-4">
-          <label>Telefono/movil</label>
-          <input type="text" />
-        </div>
-      </form>
-    </div>
-  );
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [cellPhone, setCellPhone] = useState("");
+
+	const dispatch = useDispatch();
+
+	// validaciones
+	const isFormComplete =
+		username && email && password && confirmPassword && cellPhone;
+	const isPasswordEqual = password === confirmPassword;
+
+	// submit
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		if (!isFormComplete || !isPasswordEqual) return;
+		const userData = { username, password, email, cellPhone };
+
+		dispatch(registerUser(userData));
+		dispatch(signClear(false));
+	};
+
+	return (
+		<div className="formSignIn">
+			<form onSubmit={handleSubmit}>
+				<div className="d-flex justify-content-center">
+					<button>Crear Cuenta</button>
+				</div>
+				<div className="formSignIn-usuario d-flex flex-column my-4">
+					<label>USUARIO</label>
+					<input
+						type="text"
+						value={username}
+						onChange={(event) => setUsername(event.target.value)}
+					/>
+				</div>
+				<div className="formSignIn-usuario d-flex flex-column my-4">
+					<label>CORREO ELECTRONICO</label>
+					<input
+						type="text"
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+					/>
+				</div>
+				<div className="formSignIn-contraseña d-flex flex-column my-4">
+					<label>CONTRASEÑA</label>
+					<input
+						type="pasword"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+					/>
+				</div>
+				<div className="formSignIn-usuario d-flex flex-column my-4">
+					<label>REPETIR CONTRASEÑA</label>
+					<input
+						type="text"
+						value={confirmPassword}
+						onChange={(event) =>
+							setConfirmPassword(event.target.value)
+						}
+					/>
+				</div>
+				<div className="formSignIn-usuario d-flex flex-column my-4">
+					<label>Telefono/movil</label>
+					<input
+						type="text"
+						value={cellPhone}
+						onChange={(event) => setCellPhone(event.target.value)}
+					/>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default FormSign;
